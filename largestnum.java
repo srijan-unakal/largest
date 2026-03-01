bc09fde304287e7befff1b1e4b446ee50aa3fe9c
srijan unakal <srijanunakal777@gmail.com>
2026-03-01 21:45:42 +0530

    used while loop

--- largestnum.java
+++ largestnum.java
@@ -4,6 +4,7 @@ public class largestno {
 //This program finds largest of two numbers given in the console  
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
+        while (true) { 
 		System.out.println("Enter a number : ");
 		int a= sc.nextInt();
 		System.out.println("Enter a number : ");
@@ -14,6 +15,13 @@ public class largestno {
 		else {
 			System.out.println(b+" is greater than "+a);
 		}
+        System.out.println("Do you want to continue : Y/N");
+        char c = Character.toUpperCase(sc.next().charAt(0));
+        if (c=='N'){
+            System.out.println("Thank you ");
+            break ;
+        }
+        }
 		sc.close();
 	}
 
